8e87f696dcfe5e5547926e38c32277b833b8335c
Garrett Ermer <garrettermer@Garretts-MacBook-Pro.local>
2025-08-10 16:31:19 -0600

    updating

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const sp500Box = document.getElementById("sp500");
 
 sp500Box.addEventListener("click", () => {
-    window.location.href = "ermerga.github.io/projects/stock-predictor";
+    console.log("routing to the projects");
+    window.location.href = "/projects/stock-predictor";
 })
