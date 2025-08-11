3c53a6e698bee7f3bbc49f82e417eefc453ddaeb
Garrett Ermer <garrettermer@Garretts-MacBook-Pro.local>
2025-08-10 20:59:57 -0600

    reupdating

---
+++ Result.js
@@ -0,0 +1,38 @@
+// import React, { useEffect, useState } from 'react';
+// import { useLocation } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
+
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString)
+
+const stockNameDiv = document.getElementById("stock-name");
+const closePriceDiv = document.getElementById("close-price");
+const predictionDiv = document.getElementById("prediction");
+
+
+let closePrice = "";
+let prediction = "";
+const ticker = params.get("ticker");
+let loading = true;
+
+
+
+
+stockNameDiv.innerText = "Loading Prediction...";
+
+fetch(`https://sp500backend.onrender.com/predict?ticker=${ticker}`)
+    .then(res => res.json())
+    .then(data => {
+        prediction = data.prediction;
+        closePrice = data.close;
+        
+        stockNameDiv.innerText = `Prediction for ${ticker}`;
+        closePriceDiv.innerText = `Close Price: ${closePrice}`;
+        predictionDiv.innerText = `Prediction: ${prediction}`;
+    })
+    .catch(error => {
+        console.error(error);
+        stockNameDiv.innerText = "Error fetching prediction";
+    });
+
+
---
+++ stock.js
@@ -0,0 +1,7 @@
+const tickerInput = document.getElementById("ticker-input");
+const tickerButton = document.getElementById("ticker-submit");
+
+
+tickerButton.addEventListener("click", () => {
+    window.location.href = `result.html?ticker=${tickerInput.value}`;
+})
